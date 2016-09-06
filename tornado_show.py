139da4d2e0f58ae9bc7a4f4bbcf5985ea27e884f
meng <meng>
2016-09-06 20:20:12 +0800

    edited entry_handler's re url

--- tornado_show.py
+++ tornado_show.py
@@ -12,6 +12,10 @@ class MainHandler(tornado.web.RequestHandler):
         # self.write("record is {{records}}")
         self.render("index.html", records=records)
 
+class EntryHandler(tornado.web.RequestHandler):
+	def get(self, entry_date):
+		self.render("worship.html")
+
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
     # "cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
@@ -22,9 +26,10 @@ settings = {
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler), 
+        (r"/entry/([0-9a-zA-Z-]+)", EntryHandler) # 只要url是这样的格式"/entry/([0-9a-zA-Z-]+)"，都调用EntryHandler
     ], **settings)
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8820)
+    app.listen(8832)
     tornado.ioloop.IOLoop.current().start()
